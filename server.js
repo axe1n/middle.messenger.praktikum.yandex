9b8acff53b3c6ed4bb215f63102b0b0fbea9ff39
a.schadnev <a.schadnev@diagnocat.com>
2022-11-17 22:25:37 +0400

    update env port

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = 3000;
 app.use(express.static('./dist'));
 
 app.listen(process.env.PORT || PORT, function () {
-  console.log(process.env);
+  console.log(process.env.PORT);
   console.log(`Example app listening on port ${process.env.PORT || PORT}!`);
 });
